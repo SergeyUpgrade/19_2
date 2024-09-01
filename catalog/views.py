fc10e4fac157f2d0cfa916040b13bbfd9497b8cd
Сергей <sergey.vasilev.98@mail.ru>
2024-09-01 23:08:31 +0300

    need directory templates and adding app in settings

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def index(request):
     if request.method == 'POST':
@@ -8,6 +9,7 @@ def index(request):
         message = request.POST['message']
         print(f'{name} {email} {message}')
     return render(request, 'catalog/index.html')
+
 
 def contacts(request):
     return render(request, 'catalog/contacts.html')
